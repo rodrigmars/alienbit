6a586ceee7c2ef82b58db373eebbf36900ccc3f5
rodrigmars <rodrigmars@gmail.com>
2023-08-13 12:12:12 -0300

    Modelando estrutura funcionando

--- alienbit/app.py
+++ alienbit/app.py
@@ -1,8 +1,20 @@
-def main() -> None:
-    print("Hello world")
+def hard_verse(track:str):
 
-
+    def x_play(x):
+        return y_stop
+    
+    def y_stop(x:list):
+        return z_pause
+    
+    def z_pause(z):
+        return "teste"
+    
+    return x_play
 
 if __name__ == "__main__":
 
-    main()
+    score = hard_verse("sadsa")(25)([.1, 25])(2.5)
+
+    print(score)
+
+    
