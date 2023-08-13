e2b2fe9e7152635ff22c373895938976c42b7a96
rodrigmars <rodrigmars@gmail.com>
2023-08-13 12:28:38 -0300

    Atualizando shape funcional

--- alienbit/app.py
+++ alienbit/app.py
@@ -1,14 +1,11 @@
 def hard_verse(track:str):
 
     def x_play(x):
+        def y_stop(y:list):
+            def z_pause(z):
+                return "teste"
+            return z_pause
         return y_stop
-    
-    def y_stop(x:list):
-        return z_pause
-    
-    def z_pause(z):
-        return "teste"
-    
     return x_play
 
 if __name__ == "__main__":
@@ -16,5 +13,3 @@ if __name__ == "__main__":
     score = hard_verse("sadsa")(25)([.1, 25])(2.5)
 
     print(score)
-
-    
