ada924a54891638412f3b76519d0f259901a45d5
rodrigmars <rodrigmars@gmail.com>
2023-08-15 01:14:41 -0300

    Função producer_flux encapsulada

--- alienbit/thread_capacitors.py
+++ alienbit/thread_capacitors.py
@@ -4,28 +4,27 @@ from collections import deque
 from fifo import fifo_deque
 from core import hard_verse
 
-def producer_flux(fifo_message:deque, fifo_score:deque, total_time:int):
-
-    fifo_message.appendleft((current_thread().name, 'Starting'))
-
-    time.sleep(total_time)
-
-    score = hard_verse("sadsa")(25)([.1, 25])(2.5)
-
-    fifo_score.appendleft((current_thread().name, f'score:{score}'))
-
-    time.sleep(.1)
-
-    fifo_message.appendleft((current_thread().name, 'Exiting'))
-
 def thread_capacitor() -> None:
+
+    fifo_message, fifo_score, consumer_message, consumer_score = fifo_deque()
+
+    def producer_flux(fifo_message:deque, fifo_score:deque, total_time:int):
+
+        fifo_message.appendleft((current_thread().name, 'Starting'))
+
+        time.sleep(total_time)
+
+        score = hard_verse("sadsa")(25)([.1, 25])(2.5)
+
+        fifo_score.appendleft((current_thread().name, f'score:{score}'))
+
+        time.sleep(.1)
+
+        fifo_message.appendleft((current_thread().name, 'Exiting'))
 
     capacitors:list = [[ producer_flux, 2, "flux_01"], 
                        [ producer_flux, 2, "flux_02"], 
                        [ producer_flux, 1, "flux_03"]]
-
-    fifo_message, fifo_score, consumer_message, consumer_score = fifo_deque()
-
 
     thread_message = Thread(target=consumer_message, 
                             args=(fifo_message, len(capacitors)), 
