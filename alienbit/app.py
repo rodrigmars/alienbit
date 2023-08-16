94ffc7e5d46bfbea04d7b49b479d01c570be5c39
rodrigmars <rodrigmars@gmail.com>
2023-08-16 08:44:06 -0300

    Parametro total_capacitors removido, evento implementado nos consumidores

--- alienbit/app.py
+++ alienbit/app.py
@@ -6,7 +6,27 @@ if __name__ == "__main__":
 
     try:
 
-        main()
+        from threading import Event, Thread
+        from itertools import cycle
+        import time
+        
+        event = Event()
+
+        def teste(event:Event):
+            total = 0
+            for char in cycle('◰◳◲◱'):
+                total +=1 
+                print(f"Resultado:{total:03} {char}", flush=True, end='\r')
+                if event.wait(.1):
+                # if event.is_set():
+                    break
+
+        Thread(target=teste, args=(event,)).start()
+
+        time.sleep(5)
+        event.set()
+        print("Programa finalizado")
+        # main()
 
     except Exception as e:
         print(e)
--- alienbit/fifo.py
+++ alienbit/fifo.py
@@ -1,11 +1,12 @@
 import time
+from threading import Event
 from itertools import cycle
 from collections import deque
 from infra.log_system.logging_exception import  inject_error
 
 def fifo_deque():
 
-    fifo_status_percent = deque()
+    fifo_signals_process = deque()
 
     fifo_message = deque()
 
@@ -13,7 +14,7 @@ def fifo_deque():
 
     fifo_error = deque()
 
-    def consumer_error(fifo_error:deque) -> None:
+    def consumer_error(event:Event, fifo_error:deque) -> None:
 
         while True:
 
@@ -21,28 +22,27 @@ def fifo_deque():
                 
                 inject_error(fifo_error.popleft())
 
-    def consumer_message(fifo_status_percent:deque, fifo_message:deque, total_capacitors:int) -> None:
-        
-        counter:int = 0
+            if event.wait(.1):
+                break
 
-        while counter < total_capacitors:
+    def consumer_message(event:Event, fifo_message:deque) -> None:
+
+        while True:
 
             if fifo_message:
 
                 data:tuple = fifo_message.popleft()
 
-                if "EXITING" == data[1].upper():
-                    counter += 1
-                    fifo_status_percent.appendleft(int((counter / total_capacitors) * 100))
+                # if "EXITING" == data[1].upper():
+                #     counter += 1
+                #     fifo_status_percent.appendleft(int((counter / total_capacitors) * 100))
             
-            time.sleep(0.1)
+            if event.wait(.1):
+                break
 
+    def consumer_score(event:Event, fifo_message, fifo_score:deque) -> None:
 
-    def consumer_score(fifo_message, fifo_score:deque, total_capacitors:int) -> None:
-
-        counter:int = 0 
-
-        while counter < total_capacitors:
+        while True:
 
             if fifo_score:                
 
@@ -53,31 +53,34 @@ def fifo_deque():
 
                 fifo_message.appendleft((data[0], f"Score armazenado:{data[1]}"))
 
-                counter +=1 
-
                 data.release()
 
-            time.sleep(0.1)
-                
-    def consumer_spinner(fifo_counter_processed:deque, total_capacitors:int) -> None:
-        
+            if event.wait(.1):
+                break
+
+    def consumer_spinner(event:Event, fifo_signals_process:deque, total_capacitors:int) -> None:
+
         counter_processed:int = 0
         perc: int = 0
         hide_curor:str = '\033[?25l'
         
-        for char in cycle('|/-\\'):
+        for char in cycle('◰◳◲◱'):
 
-            if fifo_counter_processed:
-                counter_processed += fifo_counter_processed.popleft()
+            if fifo_signals_process:
+                counter_processed += fifo_signals_process.popleft()
                 perc = int((counter_processed / total_capacitors) * 100)
 
-            print(end=f"{hide_curor}{'processed' if perc == 100 else 'processing'}...{char} {perc}%\r")
-            time.sleep(.08)
+            processed = 'processed' if perc == 100 else 'processing'
+
+            print(flush=True, end=f"{hide_curor}{processed}...{char} {perc}%\r")
+
+            event.wait(.1)
             
             if counter_processed >= total_capacitors:
+                event.set()
                 break
 
-    return fifo_status_percent, \
+    return fifo_signals_process, \
         fifo_message, \
             fifo_score, \
                 fifo_error, \
