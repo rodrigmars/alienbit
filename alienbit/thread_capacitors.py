bd00ee6f399ca656716225a43c98b1d7f8e4a13e
rodrigmars <rodrigmars@gmail.com>
2023-08-16 09:06:49 -0300

    Threads ajustadas

--- alienbit/thread_capacitors.py
+++ alienbit/thread_capacitors.py
@@ -1,5 +1,5 @@
 import time
-from threading import Thread, current_thread
+from threading import Event, Thread, current_thread
 from collections import deque
 from random import randrange, choice
 from fifo import fifo_deque
@@ -7,7 +7,7 @@ from core import get_estrategy, hard_verse
 
 def thread_capacitor() -> None:
     
-    fifo_status_percent, \
+    fifo_signals_process, \
         fifo_message, \
             fifo_score, \
                 fifo_error, \
@@ -17,7 +17,7 @@ def thread_capacitor() -> None:
                                 consumer_error = fifo_deque()
 
 
-    def producer_flux(fifo_message:deque, fifo_score:deque, fifo_counter_processed:deque, total_time:int, strategy:dict):
+    def producer_flux(fifo_message:deque, fifo_score:deque, fifo_signals_process:deque, total_time:int, strategy:dict):
 
         status:str=""
 
@@ -42,36 +42,36 @@ def thread_capacitor() -> None:
             status = "Processed with Error"
         finally:
             fifo_message.appendleft((current_thread().name, status))
-            fifo_counter_processed.appendleft(1)
+            fifo_signals_process.appendleft(1)
 
     TOTAL_CAPACITORS:int = 10
 
+    event:Event = Event()
+
     thread_spinner = Thread(target=consumer_spinner, 
-                            args=(fifo_status_percent,), 
+                            args=(event, fifo_signals_process, TOTAL_CAPACITORS), 
                             name="thread_spinner")
 
     thread_spinner.start()
 
-    thread_message = Thread(target=consumer_message, 
-                            args=(fifo_status_percent, fifo_message, TOTAL_CAPACITORS), 
-                            name="thread_message")
-    thread_message.start()
+    Thread(target=consumer_message, 
+                            args=(event, fifo_message), 
+                            name="thread_message").start()
     
     Thread(target=consumer_score, 
-           args=(fifo_message, fifo_score, TOTAL_CAPACITORS), 
+           args=(event, fifo_message, fifo_score), 
            name="thread_monitor").start()
 
     Thread(target=consumer_error, 
-           args=(fifo_error,), 
+           args=(event, fifo_error,), 
            name="thread_error").start()
 
     for i in range(TOTAL_CAPACITORS):
 
         Thread(target=producer_flux, 
-               args=(fifo_message, fifo_score, randrange(1, 9), TOTAL_CAPACITORS, get_estrategy()), 
+               args=(fifo_message, fifo_score, fifo_signals_process, randrange(1, 9), get_estrategy()), 
                name=f"flux_{i}").start()
         
         time.sleep(.1)
 
-    # thread_message.join()
     thread_spinner.join()
