b4f002f1c3deaf697d603e45acf98b04b684601a
rodrigmars <rodrigmars@gmail.com>
2023-08-18 15:06:23 -0300

    Ajustes para README.md + requirements.txt

--- alienbit/thread_capacitors.py
+++ alienbit/thread_capacitors.py
@@ -11,30 +11,36 @@ from consumers.flux_consumer import flux_consumer
 
 def thread_capacitor() -> None:
 
-    event:Event = Event()
+    event: Event = Event()
 
     signals_process = deque()
 
     flux_message = deque()
 
-    TOTAL_CAPACITORS:int = 10
+    TOTAL_CAPACITORS: int = 10
 
-    thread_progress = Thread(target=progress_consumer, 
-                            args=(event, signals_process, TOTAL_CAPACITORS), 
-                            name="thread_spinner")
+    thread_progress = Thread(target=progress_consumer,
+                             args=(event,
+                                   signals_process,
+                                   TOTAL_CAPACITORS),
+                             name="thread_spinner")
 
     thread_progress.start()
 
-    Thread(target=flux_consumer, 
+    Thread(target=flux_consumer,
            args=(event, flux_message, "", connection_db),
-                 name="thread_message").start()
-    
+           name="thread_message").start()
+
     for i in range(TOTAL_CAPACITORS):
 
-        Thread(target=flux_producer, 
-               args=(event, flux_message, signals_process, expedition_config, launch_expedition), 
+        Thread(target=flux_producer,
+               args=(event,
+                     flux_message,
+                     signals_process,
+                     expedition_config,
+                     launch_expedition),
                name=f"flux_{i}").start()
-        
+
         sleep(.1)
 
     thread_progress.join()
